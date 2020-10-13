6efa5f9aa3bb6ea7fc60b80aba459b09711876bd
ArnobMahmud <arnob.tech.me@gmail.com>
2020-10-13 10:06:10 +0600

    Vector in C++

---
+++ Vector/vector.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    vector <int> vec;   // initializing vector
+    cout << "Hello World" << endl;
+
+    return 0;
+}
