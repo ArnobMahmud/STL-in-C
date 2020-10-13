04de406a4662a8a30c7588e19ce190a156b648ef
ArnobMahmud <arnob.tech.me@gmail.com>
2020-10-13 15:08:17 +0600

    map pair

---
+++ Map/map_pair.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <utility>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    pair <string, int> p;
+
+    p.first = "Arnob";
+    p.second = 47;
+
+    cout << p.first << " " << p.second << endl;
+
+    return 0;
+}
